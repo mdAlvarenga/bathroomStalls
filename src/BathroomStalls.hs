934008dc8d1ba7100ccf11ea8a4653ead6bc997b
Unknown <marcos.alvarenga@10pines.com>
2018-11-28 15:17:10 -0300

    Faltan solucionar detalles para que compile.

--- src/BathroomStalls.hs
+++ src/BathroomStalls.hs
@@ -1,11 +1,13 @@
+module BathroomStalls where 
+
 type Stall = (Bool, Int) --Tupla en la que la 1er componente indica si esta ocupada o no, y la 2da indica su posicion en el baño.
 
 -- //Para cada posicion de la lista, la distancia de la izquierda.
 								-- //Este funciona como acumulador, hace cuanto tiempo vi un true
 distancesToLeft :: [Stall] -> Int -> [Int]
 distancesToLeft [] tiempoATrue = [] 
-distancesToLeft ((True, n):stalls) tiempoATrue = 0 : distancesToLeft bs 0 --//acabo de ver un true
-distancesToLeft ((False, n):stalls) tiempoATrue = tiempoATrue : distancesToLeft bs (tiempoATrue + 1)
+distancesToLeft ((True, n):stalls) tiempoATrue = 0 : distancesToLeft stalls 0 
+distancesToLeft ((False, n):stalls) tiempoATrue = tiempoATrue : distancesToLeft stalls (tiempoATrue + 1)
 
 -- al derecho hago un reverse a la lista
 
@@ -27,7 +29,7 @@ minDistance tuple = min (fst tuple) (snd tuple)
 
 maxDistances :: [(Stall, (Int, Int))] -> [(Stall, Int)]
 maxDistances [] = []
-maxDistances ((stall, distance):distances) = (maxDistance tuple) : maxDistances distances
+maxDistances ((stall, distance):distances) = (maxDistance stall) : maxDistances distances
 
 
 maxDistance :: (Int, Int) -> Int
@@ -37,21 +39,22 @@ maxDistance tuple = max (fst tuple) (snd tuple)
 maximalsOf :: [(Stall, Int)] -> [(Stall, Int)]
 maximalsOf [] = []
 maximalsOf ((stall, num) : distances) 
-	| num > head(maximalsOf distances) = [(stall, number)]
-	| num == head(maximalsOf distances) = (stall, number) : (maximalsOf distances)
+	| num > head(maximalsOf distances) = [(stall, num)]
+	| num == head(maximalsOf distances) = (stall, num) : (maximalsOf distances)
 	| num < head(maximalsOf distances) = maximalsOf distances
 
 
 chooseLeftMost :: [(Stall, Int)] -> (Stall, Int)
 chooseLeftMost xs = head xs
 
+buildStallsWith = undefined
 
 buildStalls :: Int -> [Stall]
 buildStalls quantity = (True, 0) : buildStallsWith quantity ++ [(True, quantity + 1)]
-	where 	buildStallsWith 0 = []
-			buildStallsWith q = (False, q) : buildStallsWith (q - 1)
+	-- where 	buildStallsWith 0 = []
+	-- 		buildStallsWith q = (False, q) : buildStallsWith (q - 1)
 
-			zip ([True] ++ replicate quantity False ++ [True]) [0..]
+	-- 		zip ([True] ++ replicate quantity False ++ [True]) [0..]
 
 
 returnLastStall :: [Stall] -> Stall
@@ -64,29 +67,30 @@ returnLastStall (st:sts) = returnLastStall sts
 -- ...
 
 updateStalls :: (Stall, Int) -> [Stall] -> [Stall]
-updateStalls ()
+updateStalls = undefined
 
 
 
-processForPerson :: [Stall] -> (Int, Int)
-processForPerson 
-	let stallDistances = getDistances stalls --(stall, (int, int))
-	let maximalsOfMinDistances = maximalsOf(minDistances(stallDistances))
-	-- let maximalsOfMaxDistances = maximalsOf(maxDistances(stallDistances))
-	-- let leftMost = chooseLeftMost(maxDistances)
-	if (length maximalsOfMinDistances) == 1
-		then getDistances(updateStalls(head minDistances) stalls)
-		else if (length maxDistances) == 1
-				then head maxDistances
-				else leftMost
+processForPerson :: [Stall] -> [Stall]
+processForPerson = undefined 
+	-- let stallDistances = getDistances stalls 
+	-- --(stall, (int, int))
+	-- let maximalsOfMinDistances = maximalsOf(minDistances(stallDistances))
+	-- -- let maximalsOfMaxDistances = maximalsOf(maxDistances(stallDistances))
+	-- -- let leftMost = chooseLeftMost(maxDistances)
+	-- if (length maximalsOfMinDistances) == 1
+	-- 	then getDistances(updateStalls(head minDistances) stalls)
+	-- 	else if (length maxDistances) == 1
+	-- 			then head maxDistances
+	-- 			else leftMost
 
 
 --Recibe una tupla con una cantidad de stalls y una cantidad de personas, y la procesa, retornando una tupla que en la primer coordenada tiene
 -- el minimo de los segmentos del stall para la última persona que ingresa, y en la segunda coordenada, el máximo de los segmentos para dicha persona.
 processBathroom :: (Int, Int) -> (Int, Int)
-processBathroom (stallQuantity, 0) = updateStalls(processForPerson(buildStalls(stall))) -- ???????? //ivan puto
+processBathroom (stallQuantity, 0) = updateStalls(processForPerson(buildStalls(stallQuantity))) -- ???????? //ivan puto
 processBathroom (stallQuantity, people) =  
-		let stalls = buildStalls(stallQuantity)
+		let stalls = buildStalls(stallQuantity) in 
 		processForPerson stalls
 
 
@@ -97,3 +101,7 @@ processFile :: [(Int, Int)] -> [(Int, Int)]
 processFile [] = []
 processFile (item : items) = processBathroom item : processFile items 
 
+
+
+processInput :: [String] -> [String]
+processInput = undefined
